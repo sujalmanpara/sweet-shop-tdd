7556efdb35335b7a8c953592c8b0fc0f94043fe4
Sujal <sujalmanpara@gmail.com>
2025-07-17 16:08:30 +0530

    [GREEN] Implement basic addSweet.

--- src/services/SweetShop.ts
+++ src/services/SweetShop.ts
@@ -2,6 +2,14 @@ import { Sweet } from '../models/Sweet';
 
 class SweetShop {
   private sweets: Sweet[] = [];
+
+  addSweet(sweet: Sweet) {
+    this.sweets.push(sweet);
+  }
+
+  getAllSweets(): Sweet[] {
+    return this.sweets;
+  }
 }
 
 export default SweetShop; 
