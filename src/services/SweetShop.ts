1b8b4fd46308a275d9f766ef189a3ad6947858b9
Sujal <sujalmanpara@gmail.com>
2025-07-17 16:18:15 +0530

    [REFACTOR] Optimize deleteSweet with Map.

--- src/services/SweetShop.ts
+++ src/services/SweetShop.ts
@@ -1,26 +1,24 @@
 import { Sweet } from '../models/Sweet';
 
 class SweetShop {
-  private sweets: Sweet[] = [];
+  private sweets: Map<number, Sweet> = new Map();
 
   addSweet(sweet: Sweet) {
-    if (this.sweets.some(s => s.id === sweet.id)) {
+    if (this.sweets.has(sweet.id)) {
       throw new Error('A sweet with this ID already exists.');
     }
     if (sweet.price < 0 || sweet.quantity < 0) {
       throw new Error('Price and quantity must not be negative.');
     }
-    this.sweets.push(sweet);
+    this.sweets.set(sweet.id, sweet);
   }
 
   getAllSweets(): Sweet[] {
-    return this.sweets;
+    return Array.from(this.sweets.values());
   }
 
   deleteSweet(id: number): boolean {
-    const initialLength = this.sweets.length;
-    this.sweets = this.sweets.filter(sweet => sweet.id !== id);
-    return this.sweets.length < initialLength;
+    return this.sweets.delete(id);
   }
 }
 
